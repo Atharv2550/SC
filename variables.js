ee28f06b9b7c564e33e6d348a20e47336432fe77
Atharv2550 <atharvpatil2773sjghk@gmail.com>
2026-02-02 15:44:08 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,14 @@
+
+let Name = "Atharv"
+let age = 21;              
+let isStudent = true;      
+let marks = 85;            
+const country = "India";
+
+
+
+console.log(Name);
+console.log(age);
+console.log(isStudent);
+console.log(marks);
+console.log(country);
