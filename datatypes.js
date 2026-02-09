0164731169b33e3def2344d7f585aa00396cfbbe
Atharv2550 <atharvpatil2773sjghk@gmail.com>
2026-02-09 15:10:37 +0530

    09/02

---
+++ datatypes.js
@@ -0,0 +1,11 @@
+let name = "Atharv";     
+let age = 21;            
+let isStudent = true; 
+let marks = 80;     
+let city;                
+
+console.log(typeof name);
+console.log(typeof age);      
+console.log(typeof isStudent);
+console.log(typeof marks);     
+console.log(typeof city);      
