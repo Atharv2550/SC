2644ed152f771b5cb36763d12a063d42925b3fee
Atharv2550 <atharvpatil2773sjghk@gmail.com>
2026-02-16 15:16:15 +0530

    16-02

---
+++ TYPESCRIPT/variables.js
@@ -0,0 +1,12 @@
+var studentName = "Atharv";
+var age = 21;
+var isStudent = true;
+var marks = [85, 90, 78, 88];
+var studentInfo = ["Atharv", 21];
+var collegeName = "ABC College";
+console.log("Name:", studentName);
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
+console.log("Marks:", marks);
+console.log("Student Info:", studentInfo);
+console.log("College Name:", collegeName);
---
+++ TYPESCRIPT/variables.ts
@@ -0,0 +1,18 @@
+let studentName: string = "Atharv";
+
+let age: number = 21;
+
+let isStudent: boolean = true;
+
+let marks: number[] = [85, 90, 78, 88];
+
+let studentInfo: [string, number] = ["Atharv", 21];
+
+const collegeName: string = "ABC College";
+
+
+console.log("Name:", studentName);
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
+console.log("Marks:", marks);
+console.log("Student Info:", studentInfo);
